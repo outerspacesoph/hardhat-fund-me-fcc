ee8cb370957bb2150dcc6ad1ca789f97a924a707
Sophie Lamo <sophielamo@sophies-air.lan>
2022-09-06 19:35:26 -0600

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,59 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("@nomiclabs/hardhat-ethers")
+require("dotenv").config()
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+
+/* @type import('hardhat/config').HardhatUserConfig
+ */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+
+            {
+                version: "0.6.6",
+                //settings: {},
+            },
+        ],
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 500000,
+    },
+}
